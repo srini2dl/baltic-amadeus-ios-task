69d71700f7aabe8ca13ba86ac2ef49fda8ae1293
Srinivasan <srini2dl@gmail.com>
2023-04-26 19:35:01 +0300

    Added folder structure

---
+++ Baltic Amadeus iOS Task/Baltic_Amadeus_iOS_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baltic_Amadeus_iOS_TaskApp.swift
+//  Baltic Amadeus iOS Task
+//
+//  Created by Srinivasan Raman on 2023-04-26.
+//
+
+import SwiftUI
+
+@main
+struct Baltic_Amadeus_iOS_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baltic Amadeus iOS Task/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Baltic Amadeus iOS Task
+//
+//  Created by Srinivasan Raman on 2023-04-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
